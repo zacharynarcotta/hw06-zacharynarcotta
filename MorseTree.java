12d0edb58b6e372f242d2525887fe53123ab4f21
zacharynarcotta <zachary.narcotta@maine.edu>
2025-04-23 09:48:44 -0400

    Create constructor, preorder and postorder prints

--- MorseTree.java
+++ MorseTree.java
@@ -1,3 +1,41 @@
 public class MorseTree {
+    public TreeNode<String> root;
 
+    // Default constructor for NULL root
+    public MorseTree() {
+        root = null;
+    }
+
+    public String preOrder(String out, TreeNode<String> node){ // Self -> Left -> Right
+        if (node.getLeft() == null && node.getRight() == null){
+            return node.getElement().toString();
+        }
+        else if (node.getLeft() != null && node.getRight() == null){
+            return node.getElement().toString() + " " + preOrder(out, node.getLeft());
+        }
+        else if (node.getLeft() == null && node.getRight() != null){
+            return node.getElement().toString() + " " + preOrder(out, node.getRight());
+        }
+        else{
+            return node.getElement().toString() + " " +
+            preOrder(out, node.getLeft()) + " " +
+            preOrder(out, node.getRight());
+        }
+    }
+    public String postOrder(String out, TreeNode<String> node){ // Left -> Right -> Self
+        if (node.getLeft() == null && node.getRight() == null){
+            return node.getElement().toString();
+        }
+        else if (node.getLeft() != null && node.getRight() == null){
+            return postOrder(out, node.getLeft()) + " " + node.getElement().toString();
+        }
+        else if (node.getLeft() == null && node.getRight() != null){
+            return postOrder(out, node.getRight()) + " " + node.getElement().toString();
+        }
+        else{
+            return preOrder(out, node.getLeft()) + " " +
+            preOrder(out, node.getRight()) + " " + 
+            node.getElement().toString();
+        }
+    }
 }
